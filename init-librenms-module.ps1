f0f1dacd1eadfb0fb034311055a009305b456823
Aaron Glenn <aaron@aaroneg.com>
2022-01-25 00:09:37 -0600

    test module

--- init-librenms-module.ps1
+++ init-librenms-module.ps1
@@ -8,7 +8,7 @@ catch {
     }
     $nmConfig | Export-Clixml $PSScriptRoot\lnconfig.xml
 }
-
+Import-Module librenms-rest-module
 Try {
     Import-Module CredentialManager -ErrorAction Stop
 }
@@ -16,13 +16,16 @@ Catch {
     Install-Module -Name CredentialManager -Repository PSGallery -Scope CurrentUser
 }
 
-# Get or create the API credential from the credential store
-try {$nbCred=Get-StoredCredential -Name $nmConfig.serverAddress -ErrorAction Stop}
+# Get or create the API credential
+try {$nbCred=Import-Clixml lnpassword.xml -ErrorAction Stop}
 catch {
     $nbCred=Get-Credential -Title 'LibreNMS Credentials' -Message "Credentials for $($serverAddress)" -UserName 'API'
-    New-StoredCredential -Target $nmConfig.serverAddress -Username 'API' -password $nbCred.GetNetworkCredential().Password
+    $nbCred|Export-Clixml lnpassword.xml
 }
 
 $lnConnection = New-LNMSConnection -DeviceAddress $nmConfig.serverAddress -ApiKey $nbCred.GetNetworkCredential().Password -Passthru
 Write-Output "LibreNMS connection initiated:"
 $lnConnection
+
+#$result = Test-LNMSConnection
+#($result|Get-Member -MemberType NoteProperty).Name
--- reset-module.ps1
+++ reset-module.ps1
@@ -1,2 +1,3 @@
 Remove-Module -Name librenms-rest-module -ErrorAction SilentlyContinue -Force
 Import-Module -Name librenms-rest-module -Force
+. $PSScriptRoot\init-librenms-module.ps1
